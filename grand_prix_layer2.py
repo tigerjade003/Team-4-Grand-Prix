08330cd418f433d66b9d1cc73c883671641de534
tigerjade003 <69996929+tigerjade003@users.noreply.github.com>
2024-04-25 17:32:03 -0700

    Update grand_prix_layer2.py

--- grand_prix_layer2.py
+++ grand_prix_layer2.py
@@ -3,91 +3,74 @@ MIT BWSI Autonomous RACECAR
 MIT License
 racecar-neo-outreach-labs
 
-File Name: lab_f.py
+File Name: grand_prix_layer2.py
 
-Title: Lab F - Line Follower
+Title: GRAND PRIX!!!!
 
-Author: BWSI Team 4
+Author: BWSI Team 4 2024
 
 Purpose: Write a script to enable fully autonomous behavior from the RACECAR. The
 RACECAR should automatically identify the color of a line it sees, then drive on the
-center of the line throughout the obstacle course. The RACECAR should also identify
+center of the line throughout the obstacle cour ???se. The RACECAR should also identify
 color changes, following colors with higher priority than others. Complete the lines 
 of code under the #TODO indicators to complete the lab.
 
-Expected Outcome: When the user runs the script, they are able to control the RACECAR
-using the following keys:
-- When the right trigger is pressed, the RACECAR moves forward at full speed
-- When the left trigger is pressed, the RACECAR, moves backwards at full speed
-- The angle of the RACECAR should only be controlled by the center of the line contour
-- The RACECAR sees the color RED as the highest priority, then GREEN, then BLUE
+runs as fast as possible to the finish line
 """
 
-########################################################################################
-# Imports
-########################################################################################
 
 import sys
 import cv2 as cv
 import numpy as np
 
-# If this file is nested inside a folder in the labs folder, the relative path should
-# be [1, ../../library] instead.
+
 sys.path.insert(1, "../../library")
 import racecar_core
 import racecar_utils as rc_utils
 
-########################################################################################
-# Global variables
-########################################################################################
 
+totaltime = 0
 rc = racecar_core.create_racecar()
 
-# >> Constants
-# The smallest contour we will recognize as a valid contour
 MIN_CONTOUR_AREA = 30
 
-# A crop window for the floor directly in front of the car
 CROP_FLOOR = ((360, 0), (rc.camera.get_height(), rc.camera.get_width()))
 
-# TODO Part 1: Determine the HSV color threshold pairs for GREEN and RED
-# Colors, stored as a pair (hsv_min, hsv_max) Hint: Lab E!
-BLUE = ((90, 100, 100), (120, 255, 255))  # The HSV range for the color blue
-GREEN = ((40, 100, 100), (80, 255, 255))  # The HSV range for the color green
-RED = ((0, 100, 100), (10, 255, 255))  # The HSV range for the color red
-RED2 = ((165, 100, 100), (179, 255, 255))
-
-# Color priority: Red >> GREEN >> BLUE
-COLOR_PRIORITY = (RED, RED2, GREEN, BLUE)
+BLUE = ((105, 255, 255), (105, 255, 255)) 
+GREEN = ((64, 255, 255), (64, 255, 255)) 
+RED = ((0, 255, 255), (0, 255, 255))
+COLOR_PRIORITY = (GREEN, RED, BLUE)
 
 # >> Variables
-speed = 0.0  # The current speed of the car
-angle = 0.0  # The current angle of the car's wheels
-contour_center = None  # The (pixel row, pixel column) of contour
-contour_area = 0  # The area of contour
+speed = 0.0 
+angle = 0.0  
+contour_center = None  
+contour_area = 0  
+last_color = ""
+cur_color = ""
+red = 0
+last_time = 0
+last_time2 = 0
+green = False
+greenc = 0
+found = False
 
 
-########################################################################################
-# Functions
-########################################################################################
-
-# [FUNCTION] Finds contours in the current color image and uses them to update 
-# contour_center and contour_area
 def update_contour():
+    global COLOR_PRIORITY
     global contour_center
     global contour_area
-
+    global cur_color
+    global speed
+    global angle
     image = rc.camera.get_color_image()
-
+    if(red >= 2 and cur_color != "red"):
+        COLOR_PRIORITY = (GREEN, BLUE)
     if image is None:
         contour_center = None
         contour_area = 0
     else:
-        # Crop the image to the floor directly in front of the car
         image = rc_utils.crop(image, CROP_FLOOR[0], CROP_FLOOR[1])
-
-        # TODO Part 2: Search for line colors, and update the global variables
-        # contour_center and contour_area with the largest contour found
         found = False
         for color in COLOR_PRIORITY:
             contours = rc_utils.find_contours(image, color[0], color[1])
@@ -96,121 +79,104 @@ def update_contour():
                 if not found or cv.contourArea(contour) > contour_area:
                     contour_center = rc_utils.get_contour_center(contour)
                     contour_area = rc_utils.get_contour_area(contour)
+                    found = True
+                    if color == RED:
+                        cur_color = "red"
+                    elif color == BLUE and cv.contourArea(contour) > 2000 and cv.contourArea(contour) < 3000:
+                        continue
+                    elif color == BLUE:
+                        cur_color = "blue"
+                    elif color == GREEN:
+                        cur_color = "green"
                     break
-                    
+        '''
         if found:      
             rc_utils.draw_contour(image, contour)
             rc_utils.draw_circle(image, contour_center)
+            
+    
         # Display the image to the screen
         rc.display.show_color_image(image)
+        '''
 
-# [FUNCTION] The start function is run once every time the start button is pressed
 def start():
     global speed
     global angle
-
-    # Initialize variables
+    global totaltime
+    totaltime = 0
     speed = 0
     angle = 0
 
-    # Set initial driving speed and angle
     rc.drive.set_speed_angle(speed, angle)
 
-    # Set update_slow to refresh every half second
     rc.set_update_slow_time(0.5)
 
-    # Print start message
-    print(
-        ">> Lab 2A - Color Image Line Following\n"
-        "\n"
-        "Controls:\n"
-        "   Right trigger = accelerate forward\n"
-        "   Left trigger = accelerate backward\n"
-        "   A button = print current speed and angle\n"
-        "   B button = print contour center and area\n"
-        "   X Button = change COLOR_PRIORITY to  [RED, BLUE, GREEN] \n"
-        "   Y Button = change COLOR_PRIORITY to  [GREEN, BLUE, RED] \n"
-        "   UP/DOWN Arrow = change COLOR_PRIORITY to [BLUE, RED, GREEN] \n"
-        "   LEFT/RIGHT Arrow = change COLOR_PRIORITY to [GREEN, RED, BLUE] \n"
-        "   A/D keys = change COLOR_PRIORITY to [BLUE, GREEN, RED]\n"
-        "   W/S keys = change COLOR_PRIORITY to [RED, GREEN, BLUE](default) \n"
-    )
 
-# [FUNCTION] After start() is run, this function is run once every frame (ideally at
-# 60 frames per second or slower depending on processing speed) until the back button
-# is pressed  
+
 def remap_range(val: float, old_min: float, old_max: float, new_min: float, new_max: float) -> float:
     old_range = old_max - old_min
     new_range = new_max - new_min
     return new_range * (float(val - old_min) / float(old_range)) + new_min
 def update():
-    """
-    After start() is run, this function is run every frame until the back button
-    is pressed
-    """
+    global totaltime
+    global last_color
+    global red
+    global green
+    global last_time
+    global last_time2
     global COLOR_PRIORITY
     global speed
     global angle
     global contour_area
-    # Search for contours in the current color image
+    global found
+    global greenc
+    totaltime += rc.get_delta_time()
+    if green and greenc == 1:
+        rc.drive.set_max_speed(0.36)
+    elif red == 1:
+        rc.drive.set_max_speed(0.4)
+    else:
+        rc.drive.set_max_speed(0.5)
     contour_area = 0
     update_contour()
+    if cur_color != last_color:
+        last_color = cur_color
+        if cur_color == "red":
+            red += 1
+        if cur_color == "green":
+            green = True
+            greenc += 1
+        else:
+            green = False
 
-    # TODO Part 3: Determine the angle that the RACECAR should receive based on the current 
-    # position of the center of line contour on the screen. Hint: The RACECAR should drive in
-    # a direction that moves the line back to the center of the screen.
-
-    # Choose an angle based on contour_center
-    # If we could not find a contour, keep the previous angle
     if contour_center is not None:
         setpoint = rc.camera.get_width() // 2
         error = -(setpoint - contour_center[1])
-        #print(f"ERROR: {error}")
         angle = remap_range(error, -rc.camera.get_width()/2, rc.camera.get_width()/2, -1, 1)
-        #print(f"ANGLE: {angle}")
+        if((contour_area > 7700 and not found) or (totaltime - 0.7 <= last_time and last_time != 0)):
+            if(angle < -0.6 or angle > -0.3):
+                angle = abs(angle)
+            if contour_area > 7700:
+                last_time = totaltime
+                found = True
 
-    # Use the triggers to control the car's speed
     if contour_area > 0:
-        speed = 0.4
+        speed = 1
     else:
-        speed = 0
+        speed = -1
+        angle = 1
+    
     rc.drive.set_speed_angle(speed, angle)
 
-    # Print the current speed and angle when the A button is held down
     if rc.controller.is_down(rc.controller.Button.A):
-        print("Speed:", speed, "Angle:", angle)
-
-    # Print the center and area of the largest contour when B is held down
+        print("Speed:", speed, "Angle:", angle, "GREEN:", green, "RED:", red, "COLOR:", cur_color)
     if rc.controller.is_down(rc.controller.Button.B):
         if contour_center is None:
             print("No contour found")
         else:
             print("Center:", contour_center, "Area:", contour_area)
     
-    if rc.controller.is_down(rc.controller.Button.X):
-        COLOR_PRIORITY = [RED, RED2, BLUE, GREEN]
-        print("COLOR_PRIORITY = [RED, BLUE, GREEN]")
-    if rc.controller.is_down(rc.controller.Button.Y):
-        COLOR_PRIORITY = [GREEN, BLUE, RED, RED2]
-        print("COLOR_PRIORITY = [GREEN, BLUE, RED]")
-    a, b = rc.controller.get_joystick(rc.controller.Joystick.RIGHT)
-    if a != 0:
-        print("COLOR_PRIORITY = [GREEN, RED, BLUE]")
-        COLOR_PRIORITY = [GREEN, RED, RED2, BLUE]
-    if b != 0:
-        print("COLOR_PRIORITY = [BLUE, RED, GREEN]")
-        COLOR_PRIORITY = [BLUE, RED, RED2, GREEN]
-    x, y = rc.controller.get_joystick(rc.controller.Joystick.LEFT)
-    if x != 0:
-        print("COLOR_PRIORITY = [BLUE, GREEN, RED]")
-        COLOR_PRIORITY = [BLUE, GREEN, RED, RED2]
-    if y != 0:
-        print("COLOR_PRIORITY = [RED, GREEN, BLUE]")
-        COLOR_PRIORITY = [RED, RED2, GREEN, BLUE]
 
-# [FUNCTION] update_slow() is similar to update() but is called once per second by
-# default. It is especially useful for printing debug messages, since printing a 
-# message every frame in update is computationally expensive and creates clutter
 def update_slow():
     ###
     '''
